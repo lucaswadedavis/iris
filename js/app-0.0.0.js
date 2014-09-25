98ebcbbd1c20e10a6925b1b38d099350be1cb086
Luke Davis <lukefable@gmail.com>
2014-09-25 15:40:40 +0000

    got a click listener working, but for some reason zi's not updating.

--- js/app-0.0.0.js
+++ js/app-0.0.0.js
@@ -1,33 +1,51 @@
-$(document).ready(function(){
-app.c.init();
-});
-
+$(document).ready(function(){ app.c.init(); });
 /////////////////////////////////////////////
 /////////////////////////////////////////////
-
-
 var app={};
 app.m={};
 app.v={};
 app.c={};
-
-
 /////////////////////////////////////////////
 /////////////////////////////////////////////
 
-
-
 /////////////////////////////////////////////
 /////////////////////////////////////////////
-
 app.c.init=function(){
+    Parse.initialize("EHse3gUBrkDSdmoAv0vUt269OLvBlIxDh54gQhYx", "oAXUTPxxFBfsaEjn2E8eOD3UwHyzAJReAga8opsH");
     app.v.init();
 };
 
+app.c.create_object=function(opts){
+    var TestObject = Parse.Object.extend("TestObject");
+    var testObject = new TestObject();
+    testObject.save({foo: "bar"}).then(function(object) {
+        alert("yay! it worked");
+        });
+};
+
+app.c.create_user=function(opts){
+    var user = new Parse.User();
+    
+    user.set("username", opts.username);
+    user.set("password", opts.password);
+    user.set("email", opts.email);
+    // other fields can be set just like with Parse.Object
+    user.set("phone", opts.phone);
+    user.set("color", opts.color);
+    
+    user.signUp(null, {
+      success: function(user) {
+        // Hooray! Let them use the app now.
+      },
+      error: function(user, error) {
+        // Show the error message somewhere and let the user try again.
+        alert("Error: " + error.code + " " + error.message);
+      }
+    });
+};
 
 /////////////////////////////////////////////
 /////////////////////////////////////////////
-
 app.v.init=function(){
     zi.css();
     app.v.LAYOUT();
@@ -36,7 +54,10 @@ app.v.init=function(){
 app.v.LAYOUT=function(){
     var d="<div id='canvas'></div>";
     $("body").html(d);    
-    app.v.IRIS("canvas");
+    var b=davis.quickBox();
+    var c=new Raphael("canvas");
+    
+    app.v.RAIN(c,b);
 };
 
 app.v.IRIS=function(target){
@@ -60,6 +81,19 @@ app.v.IRIS=function(target){
     opts.canvas=c;
     
     geo.donut(opts);
+};
+
+app.v.RAIN=function(canvas,bounds){
+    var c=canvas;
+    var b=bounds;
+    
+    $('#canvas').mousedown(function (e) { //Relative ( to its parent) mouse position 
+        var posX = e.pageX;
+        var posY = e.pageY;
+        var circle=c.circle(posX,posY,0).attr({"opacity":1,"fill":"#fff"});
+        circle.animate({"r":200,"opacity":0},3000,">",function(){this.remove();});  
+        });
+    
 };
 
 /////////////////////////////////////////////
--- js/zi.js
+++ js/zi.js
@@ -9,7 +9,9 @@ zi.config=function(){
           "padding":"0",
           "margin":"0",
           "width":"100%",
-          "height":"100%"
+          "height":"100%",
+          "background":"#333",
+          "color":"#fff"
       },
       "div#canvas":{
           "padding":"0px",
